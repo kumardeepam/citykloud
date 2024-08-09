2af41bb442edf28a19f3c49c60534269c3cedf51
Kumar <kumar.deepam@gmail.com>
2024-08-09 18:36:36 +0530

    add reqrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: '/:slug*',
+        destination: 'https://tools.citykloud.com/:slug*',
+      },
+      {
+        source: '/web-stories/:slug*',
+        destination: 'https://citykloud.com/web-stories/:slug*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
