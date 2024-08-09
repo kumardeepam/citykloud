2872685f581dc180beabf6fc3efec38ffcd71be3
Kumar <kumar.deepam@gmail.com>
2024-08-10 01:49:58 +0530

    add reqrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/tools',
-        destination: 'https://tools.citykloud.com/',
-      },
-      {
-        source: '/tool/:slug*',
-        destination: 'https://tools.citykloud.com/tool/:slug*',
-      },
-    ];
-  },
-  // allow origins
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
